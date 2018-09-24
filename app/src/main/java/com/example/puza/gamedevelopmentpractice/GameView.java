1360678d9819239f2ddc69d6695bb2b187930557
Puja <csta.puja@gmail.com>
2018-09-24 12:52:11 +0545

    enemies added

--- app/src/main/java/com/example/puza/gamedevelopmentpractice/GameView.java
+++ app/src/main/java/com/example/puza/gamedevelopmentpractice/GameView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -12,96 +13,99 @@ import java.util.ArrayList;
 
 public class GameView extends SurfaceView implements Runnable {
 
-    //boolean variable to track if the game is playing or not
-    volatile Boolean playing;
-
-    //the game thread
+    volatile boolean playing;
     private Thread gameThread = null;
-
-    //adding player to this class
     private Player player;
 
-    //these objects will be used for drawing
     private Paint paint;
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
 
-    //adding an stars list
-    private ArrayList<Star> stars = new ArrayList<Star>();
+    //Adding enemies object array
+    private Enemy[] enemies;
 
-    //Class constructor
+    //Adding 3 enemies you may increase the size
+    private int enemyCount = 3;
+
+    private ArrayList<Star> stars = new
+            ArrayList<Star>();
+
     public GameView(Context context, int screenX, int screenY) {
         super(context);
-
-        //initializing player object
-        //this time also passing screen size to player constructor
         player = new Player(context, screenX, screenY);
 
-        //initalizing drawing objects
         surfaceHolder = getHolder();
         paint = new Paint();
 
-        //adding 100 stars you may increase the number
         int starNums = 100;
         for (int i = 0; i < starNums; i++) {
             Star s = new Star(screenX, screenY);
             stars.add(s);
+        }
+
+        //initializing enemy object array
+        enemies = new Enemy[enemyCount];
+        for(int i=0; i<enemyCount; i++){
+            enemies[i] = new Enemy(context, screenX, screenY);
         }
     }
 
     @Override
     public void run() {
         while (playing) {
-
-            //to update the frame
             update();
-
-            //to draw the frame
             draw();
-
-            //to control
             control();
         }
     }
 
     private void update() {
-
-        //updating player position
         player.update();
-
-        //updating teh star with player speed
         for (Star s : stars) {
             s.update(player.getSpeed());
         }
+
+        for(int i=0; i<enemyCount; i++){
+            enemies[i].update(player.getSpeed());
+
+            //if collision occurrs with player
+            if (Rect.intersects(player.getDetectCollision(), enemies[i].getDetectCollision())) {
+                //moving enemy outside the left edge
+                enemies[i].setX(-200);
+            }
+        }
     }
 
-    private void draw(){
-
-        //checkinh if surface is valid
+    private void draw() {
         if (surfaceHolder.getSurface().isValid()) {
-
-            //locking the canvas
             canvas = surfaceHolder.lockCanvas();
-            //drawing a background color for canvas
             canvas.drawColor(Color.BLACK);
 
-            //setting the paint color to white to draw the stars
             paint.setColor(Color.WHITE);
 
-            //drawing all stars
             for (Star s : stars) {
                 paint.setStrokeWidth(s.getStarWidth());
                 canvas.drawPoint(s.getX(), s.getY(), paint);
             }
 
-            //drawing the player
             canvas.drawBitmap(
                     player.getBitmap(),
                     player.getX(),
                     player.getY(),
                     paint);
-            //Unlocking the canvas
+
+            //drawing the enemies
+            for (int i = 0; i < enemyCount; i++) {
+                canvas.drawBitmap(
+                        enemies[i].getBitmap(),
+                        enemies[i].getX(),
+                        enemies[i].getY(),
+                        paint
+                );
+            }
+
             surfaceHolder.unlockCanvasAndPost(canvas);
+
         }
     }
 
@@ -114,38 +118,30 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     public void pause() {
-        //when the game is paused
-        //setting the variable tp false
         playing = false;
         try {
-            //stopping the thread
             gameThread.join();
         } catch (InterruptedException e) {
-
         }
     }
 
-    public void resume(){
-        //when the game is resumed
-        //starting the thread again
+    public void resume() {
         playing = true;
         gameThread = new Thread(this);
         gameThread.start();
     }
 
+
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction() & MotionEvent.ACTION_MASK){
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_UP:
-                //stopping the boosting when screen is released
                 player.stopBoosting();
                 break;
             case MotionEvent.ACTION_DOWN:
-                //boosting the space jet when screen is pressed
                 player.setBoosting();
                 break;
         }
-
         return true;
     }
 }
--- app/src/main/java/com/example/puza/gamedevelopmentpractice/Player.java
+++ app/src/main/java/com/example/puza/gamedevelopmentpractice/Player.java
@@ -3,96 +3,79 @@ package com.example.puza.gamedevelopmentpractice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 
 public class Player {
-
-    //Bitmap to get character from image
     private Bitmap bitmap;
-
-    //coordinates
     private int x;
     private int y;
-
-    //motion speed of the character
     private int speed = 0;
-
-    //boolean variable to track the ship is boosting or not
     private boolean boosting;
-
-    //gravity values to add gravity effect on the ship
     private final int GRAVITY = -10;
-
-    //Controlling Y coordinate so that ship won't go outside teh screen
     private int maxY;
     private int minY;
 
-    //Limit the bounds of the ship's speed
     private final int MIN_SPEED = 1;
     private final int MAX_SPEED = 20;
 
+    private Rect detectCollision;
 
-    //constructor
     public Player(Context context, int screenX, int screenY) {
         x = 75;
         y = 50;
         speed = 1;
-
-        //getting bitmap from drawable resource
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.player);
-
-        //calculating maxY
         maxY = screenY - bitmap.getHeight();
-
-        //top edge's y paint is 0 so min y will always be zero
         minY = 0;
-
-        //seeting the boosting values to false initially
         boosting = false;
+
+        //initializing rect object
+        detectCollision =  new Rect(x, y, bitmap.getWidth(), bitmap.getHeight());
     }
 
-    //setting boosting true
     public void setBoosting() {
         boosting = true;
     }
 
-    //setting boosting false
     public void stopBoosting() {
-        //if the ship is boosting
+        boosting = false;
+    }
+
+    public void update() {
         if (boosting) {
-            //speeding up the ship
             speed += 2;
         } else {
-            //slowing down if not boosting
             speed -= 5;
         }
 
-        //controlling the top speed
         if (speed > MAX_SPEED) {
             speed = MAX_SPEED;
         }
 
-        //if the speed is less than min speed
-        //controlling it so that it won't stop completely
         if (speed < MIN_SPEED) {
             speed = MIN_SPEED;
         }
 
-        //moving the ship down
         y -= speed + GRAVITY;
 
-        //but controlling it also so that it won't go off the screen
         if (y < minY) {
             y = minY;
         }
-        if (y > minY) {
-            y =maxY;
+        if (y > maxY) {
+            y = maxY;
         }
+
+        //adding top, left, bottom and right to the rect object
+        detectCollision.left = x;
+        detectCollision.top = y;
+        detectCollision.right = x + bitmap.getWidth();
+        detectCollision.bottom = y + bitmap.getHeight();
+
     }
 
-    //method to update coordinate of character
-    public void update(){
-        //updating x coordinate
-        x++;
+    //one more getter for getting the rect object
+    public Rect getDetectCollision() {
+        return detectCollision;
     }
 
     public Bitmap getBitmap() {
