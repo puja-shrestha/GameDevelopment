0f65d58d4e7b257ed12a31cc4f78f1fe6cd046cc
Puja <csta.puja@gmail.com>
2018-09-24 15:35:06 +0545

    scores added

--- app/src/main/java/com/example/puza/gamedevelopmentpractice/GameView.java
+++ app/src/main/java/com/example/puza/gamedevelopmentpractice/GameView.java
@@ -58,6 +58,16 @@ public class GameView extends SurfaceView implements Runnable {
     private boolean isGameOver ;
     /*-----------for game over-----------------*/
 
+    /*----------Adding Scores------------------*/
+    //the score holder
+    int score;
+
+    //the high Scores Holder
+    int highScore[] = new int[4];
+
+    //Shared Prefernces to store the High Scores
+    SharedPreferences sharedPreferences;
+    /*-----------------------------------------*/
 
     public GameView(Context context, int screenX, int screenY) {
         super(context);
@@ -91,6 +101,21 @@ public class GameView extends SurfaceView implements Runnable {
 
         /*-----------for game over-----------------*/
 
+        /*-----------Adding scores-----------------*/
+
+        //setting the score to 0 initially
+        score = 0;
+
+        sharedPreferences = context.getSharedPreferences("SHAR_PREF_NAME",Context.MODE_PRIVATE);
+
+        //initializing the array high scores with the previous values
+        highScore[0] = sharedPreferences.getInt("score1",0);
+        highScore[1] = sharedPreferences.getInt("score2",0);
+        highScore[2] = sharedPreferences.getInt("score3",0);
+        highScore[3] = sharedPreferences.getInt("score4",0);
+
+        /*-----------Adding scores-----------------*/
+
     }
 
 
@@ -104,6 +129,9 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void update() {
+        //incrementing score as time passes
+        score++;
+
         player.update();
 
         //setting boom outside the screen
@@ -111,6 +139,7 @@ public class GameView extends SurfaceView implements Runnable {
         boom.setY(-250);
 
         for (Star s : stars) {
+
             s.update(player.getSpeed());
         }
 
@@ -122,29 +151,55 @@ public class GameView extends SurfaceView implements Runnable {
         enemies.update(player.getSpeed());
         //if collision occurs with player
         if (Rect.intersects(player.getDetectCollision(), enemies.getDetectCollision())) {
+
             //displaying boom at that location
             boom.setX(enemies.getX());
             boom.setY(enemies.getY());
-            //will play a sound at the collision between player and the enemy
+
+
+            //playing a sound at the collision between player and the enemy
 
 
             enemies.setX(-200);
         }// the condition where player misses the enemy
         else{
+
             //if the enemy has just entered
             if(flag){
+
                 //if player's x coordinate is more than the enemies's x coordinate.i.e. enemy has just passed across the player
-                if(player.getDetectCollision().exactCenterX() >= enemies.getDetectCollision().exactCenterX()){
+                if(player.getDetectCollision().exactCenterX()>=enemies.getDetectCollision().exactCenterX()){
+
                     //increment countMisses
                     countMisses++;
+
 
                     //setting the flag false so that the else part is executed only when new enemy enters the screen
                     flag = false;
                     //if no of Misses is equal to 3, then game is over.
                     if(countMisses==3){
+
                         //setting playing false to stop the game.
                         playing = false;
                         isGameOver = true;
+
+                        //Assigning the scores to the highscore integer array
+                        for(int i=0;i<4;i++){
+                            if(highScore[i]<score){
+
+                                final int finalI = i;
+                                highScore[i] = score;
+                                break;
+                            }
+                        }
+
+                        //storing the scores through shared Preferences
+                        SharedPreferences.Editor e = sharedPreferences.edit();
+                        for(int i=0;i<4;i++){
+                            int j = i+1;
+                            e.putInt("score"+j,highScore[i]);
+                        }
+                        e.apply();
                     }
                 }
             }
@@ -162,6 +217,24 @@ public class GameView extends SurfaceView implements Runnable {
             playing = false;
             //setting the isGameOver true as the game is over
             isGameOver = true;
+
+            //Assigning the scores to the highscore integer array
+            for(int i=0;i<4;i++){
+                if(highScore[i]<score){
+
+                    final int finalI = i;
+                    highScore[i] = score;
+                    break;
+                }
+            }
+
+            //storing the scores through shared Preferences
+            SharedPreferences.Editor e = sharedPreferences.edit();
+            for(int i=0;i<4;i++){
+                int j = i+1;
+                e.putInt("score"+j,highScore[i]);
+            }
+            e.apply();
         }
     }
 
@@ -170,6 +243,7 @@ public class GameView extends SurfaceView implements Runnable {
             canvas = surfaceHolder.lockCanvas();
             canvas.drawColor(Color.BLACK);
 
+
             paint.setColor(Color.WHITE);
             paint.setTextSize(20);
 
@@ -177,6 +251,10 @@ public class GameView extends SurfaceView implements Runnable {
                 paint.setStrokeWidth(s.getStarWidth());
                 canvas.drawPoint(s.getX(), s.getY(), paint);
             }
+
+            //drawing the score on the game screen
+            paint.setTextSize(30);
+            canvas.drawText("Score:"+score,100,50,paint);
 
             canvas.drawBitmap(
                     player.getBitmap(),
@@ -192,6 +270,7 @@ public class GameView extends SurfaceView implements Runnable {
                     paint
             );
 
+
             //drawing boom image
             canvas.drawBitmap(
                     boom.getBitmap(),
@@ -199,6 +278,7 @@ public class GameView extends SurfaceView implements Runnable {
                     boom.getY(),
                     paint
             );
+
 
             //drawing friends image
             canvas.drawBitmap(
@@ -217,7 +297,9 @@ public class GameView extends SurfaceView implements Runnable {
                 int yPos=(int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2));
                 canvas.drawText("Game Over",canvas.getWidth()/2,yPos,paint);
             }
+
             surfaceHolder.unlockCanvasAndPost(canvas);
+
         }
     }
 
