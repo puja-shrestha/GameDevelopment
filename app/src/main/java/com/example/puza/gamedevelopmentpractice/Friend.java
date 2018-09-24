fd59f01224777d65484cc3ce7aeeec60ca4a069f
Puja <csta.puja@gmail.com>
2018-09-24 13:42:44 +0545

    detecting collision between player and friend ships to make game over

--- app/src/main/java/com/example/puza/gamedevelopmentpractice/Friend.java
+++ app/src/main/java/com/example/puza/gamedevelopmentpractice/Friend.java
@@ -19,6 +19,9 @@ public class Friend {
     private int maxY;
     private int minY;
 
+    //creating a rect object for a friendly ship
+    private Rect detectCollision;
+
 
     public Friend(Context context, int screenX, int screenY) {
         bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.friend);
@@ -31,6 +34,8 @@ public class Friend {
         x = screenX;
         y = generator.nextInt(maxY) - bitmap.getHeight();
 
+        //initializing rect object
+        detectCollision = new Rect(x, y, bitmap.getWidth(), bitmap.getHeight());
     }
 
     public void update(int playerSpeed) {
@@ -42,11 +47,19 @@ public class Friend {
             x = maxX;
             y = generator.nextInt(maxY) - bitmap.getHeight();
         }
+
+        //Adding the top, left, bottom and right to the rect object
+        detectCollision.left = x;
+        detectCollision.top = y;
+        detectCollision.right = x + bitmap.getWidth();
+        detectCollision.bottom = y + bitmap.getHeight();
     }
+
+
 
     //one more getter for getting the rect object
     public Rect getDetectCollision() {
-        return getDetectCollision();
+        return detectCollision;
     }
 
     //getters
